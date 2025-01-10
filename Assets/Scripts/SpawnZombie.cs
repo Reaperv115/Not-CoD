e03c82349eaf375cab6122af54bd669a6c6cd995
Slaygony <rjs5785@hotmail.com>
2025-01-10 12:39:35 -0500

    zombie spawns and goes towards the player

---
+++ Assets/Scripts/SpawnZombie.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SpawnZombie : MonoBehaviour
+{
+     GameObject zombie, zombieClone;
+    int numzombiestoSpawn;
+    float spawnTimer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        zombie = Resources.Load<GameObject>("Zombie");
+        spawnTimer = 5;
+        numzombiestoSpawn = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer <= 0)
+            {
+                if (numzombiestoSpawn > 0)
+                {
+                    zombieClone = Instantiate(zombie, transform.position, Quaternion.identity);
+                    spawnTimer = 5;
+                }
+            }
+            else
+            {
+                Debug.Log(spawnTimer);
+                spawnTimer -= Time.deltaTime;
+            }
+    }
+}
